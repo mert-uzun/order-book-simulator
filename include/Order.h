5888ae46d87e58f013d799494029efab3ed13907
Mert Uzun <mert.uzun@ug.bilkent.edu.tr>
2025-09-27 18:35:31 +0300

    Implemented Order struct

---
+++ include/Order.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <string>
+
+const double tick_size = 0.01;
+
+struct Order {      
+        int64_t id;
+        bool isBuy;
+        bool isActive;
+        int64_t price_tick;
+        int quantity;
+        int64_t ts_created_us;
+        int64_t ts_last_update_us;
+};
