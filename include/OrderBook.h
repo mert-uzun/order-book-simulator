dbe40e2898327df332231d31d8b648f98d8727e5
Mert Uzun <mert.uzun@ug.bilkent.edu.tr>
2025-09-28 13:35:29 +0300

    Implemented OrderBook.h

--- include/OrderBook.h
+++ include/OrderBook.h
@@ -1,4 +1,25 @@
 #pragma once
 
-#include <map>
-#include <vector>
+#include <unordered_map>
+#include <list>
+#include <queue>
+#include "Order.h"
+#include "Trade.h"
+
+class OrderBook {
+    private:
+        std::unordered_map<int64_t, std::list<Order>> buys;
+        std::unordered_map<int64_t, std::list<Order>> sells;
+        std::unordered_map<int64_t, std::tuple<int64_t, std::list<Order>::iterator> order_lookup;
+        
+    public:
+        OrderBook();
+        void addLimitOrder();
+        void addMarketOrder();
+        void calcelOrder();
+        void modifyOrder();
+        void partialFill();
+        int64_t getBestBid();
+        int64_t getBestAsk();
+        void snapshot();
+}
