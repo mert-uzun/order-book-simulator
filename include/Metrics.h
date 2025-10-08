21575500e9004d38256a16f4e9266386ac8b3dfc
Mert Uzun <mert.uzun@ug.bilkent.edu.tr>
2025-10-08 22:43:22 +0300

    finished implementing on_market_price_change, on_fill and take_screenshot methods

--- include/Metrics.h
+++ include/Metrics.h
@@ -38,6 +38,11 @@ class Metrics {
         long long realized_pnl_ticks;
         long long unrealized_pnl_ticks;
         long long total_pnl_ticks;
+        std::vector<long long> timestamp_series;
+        std::vector<long long> total_pnl_ticks_series;
+        std::vector<long long> realized_pnl_ticks_series;
+        std::vector<long long> unrealized_pnl_ticks_series;
+        std::vector<long long> market_price_ticks_series;
 
         long gross_traded_qty;
         long resting_attempted_qty;
@@ -49,7 +54,7 @@ class Metrics {
         long long max_dropdown_ticks;
 
         std::vector<double> returns_series;
-        long long current_return_bucket_start_us;
+        long long last_return_bucket_start_us;
         long long last_marked_total_pnl_ticks;
 
         long long current_best_bid_price_ticks;
@@ -82,12 +87,13 @@ class Metrics {
 
         void set_config(double tick_size, long long maker_rebate_per_share_ticks, long long taker_fee_per_share_ticks, MarkingMethod marking_method, long long return_bucket_interval_us);
         void reset();
-        void finalize();
+        void finalize(long long timestamp);
         void on_order_placed(long long order_id, Side side, long long arrival_price_ticks, long long arrival_timestamp_us, int intended_quantity, bool is_instant);
         void on_order_cancelled(long long order_id, long long delete_timestamp_us);
         void on_fill(long long order_id_1, long long fill_price_ticks, long long fill_timestamp_us, int filled_quantity, bool was_instant);
-        void on_market_price_update(long long timestamp_us);
+        void on_market_price_update(long long timestamp_us, long long best_bid, long long best_ask);
         void update_last_mark_price();
+        void take_screenshot(long long timestamp, bool is_final);
 
         int get_position();
         long long get_avg_entry_price_ticks();
--- src/Metrics.cpp
+++ src/Metrics.cpp
@@ -1,13 +1,17 @@
 #include "../include/Metrics.h"
+#include <algorithm>
 #include <cmath>
 #include <cstdlib>
-
-Metrics::Metrics() : config(0, 0, 0, 0, MarkingMethod::MID) {
-    reset();
-}
+#include <iostream>
+#include <stdexcept>
 
 const int Metrics::TRADING_DAYS_PER_YEAR = 252;
 const double Metrics::HOURS_PER_DAY = 6.5;
+
+Metrics::Metrics() : config(0, 0, 0, 0, MarkingMethod::MID), timestamp_series(), total_pnl_ticks_series(), 
+                    realized_pnl_ticks_series(), unrealized_pnl_ticks_series(), market_price_ticks_series(), returns_series(), order_cache() {
+    reset();
+}
 
 void Metrics::set_config(double tick_size, long long maker_rebate_per_share_ticks, long long taker_fee_per_share_ticks, MarkingMethod marking_method, long long return_bucket_interval_us) {
     config.tick_size = tick_size;
@@ -26,6 +30,12 @@ void Metrics::reset() {
     realized_pnl_ticks = 0;
     unrealized_pnl_ticks = 0;
     total_pnl_ticks = 0;
+    timestamp_series.clear();
+    total_pnl_ticks_series.clear();
+    realized_pnl_ticks_series.clear();
+    unrealized_pnl_ticks_series.clear();
+    market_price_ticks_series.clear();
+
 
     gross_traded_qty = 0;
     resting_attempted_qty = 0;
@@ -36,7 +46,7 @@ void Metrics::reset() {
     max_dropdown_ticks = 0;
 
     returns_series.clear();
-    current_return_bucket_start_us = 0;
+    last_return_bucket_start_us = 0;
     last_marked_total_pnl_ticks = 0;
 
     current_best_ask_price_ticks = 0;
@@ -53,24 +63,8 @@ void Metrics::reset() {
     win_rate = 0;
 }
 
-void Metrics::finalize() {
-    // Update unrealized & total PnL one last time with latest values
-    update_last_mark_price();
-
-    unrealized_pnl_ticks = position * (last_mark_price_ticks - average_entry_price_ticks);
-    total_pnl_ticks = realized_pnl_ticks + unrealized_pnl_ticks + fees_ticks;
-
-    // Check max_equity and max_dropdown, update if there are changes
-    if (total_pnl_ticks > equity_value_peak_ticks) {
-        equity_value_peak_ticks = total_pnl_ticks;
-    }
-    else if (total_pnl_ticks - equity_value_peak_ticks < max_dropdown_ticks) {
-        max_dropdown_ticks = total_pnl_ticks - equity_value_peak_ticks;
-    }
-
-    // Process the last returns bucket
-    returns_series.push_back(total_pnl_ticks - last_marked_total_pnl_ticks);
-    last_marked_total_pnl_ticks = total_pnl_ticks;
+void Metrics::finalize(long long timestamp) {
+    take_screenshot(timestamp, true);
 
     // Calculate summary statistics
     double returns_mean = 0;
@@ -137,9 +131,9 @@ void Metrics::on_order_cancelled(long long order_id, long long delete_timestamp_
     order_cache.erase(order_id);
 }
 
-void Metrics::on_fill(long long order_id_1, long long fill_price_ticks, long long fill_timestamp_us, int filled_quantity, bool was_instant) {
+void Metrics::on_fill(long long order_id_1, long long fill_price_per_share_ticks, long long fill_timestamp_us, int filled_quantity, bool was_instant) {
     gross_traded_qty += filled_quantity;
-    last_trade_price_ticks = fill_price_ticks;
+    last_trade_price_ticks = fill_price_per_share_ticks;
     
     if (!was_instant) {
         resting_filled_qty += filled_quantity;
@@ -150,50 +144,88 @@ void Metrics::on_fill(long long order_id_1, long long fill_price_ticks, long lon
     }
 
     auto iter_order_1 = order_cache.find(order_id_1);
+    if (iter_order_1 == order_cache.end()) {
+        std::cout << "There is no such order with given id (" << order_id_1 << ") to fill in the cache.";
+        return;
+    }
+
     Side side = iter_order_1->second.side;
 
     long long arrival_price = iter_order_1->second.arrival_mark_price_ticks;
     long long slippage_per_share;
-    side == Side::BUYS ? slippage_per_share = fill_price_ticks - arrival_price : slippage_per_share = arrival_price - fill_price_ticks;
+    side == Side::BUYS ? slippage_per_share = fill_price_per_share_ticks - arrival_price : slippage_per_share = arrival_price - fill_price_per_share_ticks;
     total_slippage_ticks += slippage_per_share * filled_quantity;
 
+    
     if (position >= 0) {
-        if (side == Side::BUYS) {
-            
+        if (side == Side::BUYS) { // Means it is either opening or increasing a position
+            average_entry_price_ticks = ((position * average_entry_price_ticks) + (fill_price_per_share_ticks * filled_quantity)) / (position + filled_quantity);
+            position += filled_quantity;
         }
-        else {
-        
+        else { // Means it is either reducing or closing a position
+            realized_pnl_ticks += std::min(position, filled_quantity) * (fill_price_per_share_ticks - average_entry_price_ticks);
+            position -= filled_quantity;
+
+            if (position < 0) {
+                average_entry_price_ticks = fill_price_per_share_ticks;
+            }
         }
     }
     else {
-        
+        if (side == Side::SELLS) { // Means it is either opening or increasing a position
+            average_entry_price_ticks = ((abs(position) * average_entry_price_ticks) + (fill_price_per_share_ticks * filled_quantity)) / abs(position - filled_quantity);
+            position -= filled_quantity;
+        }
+        else { // Means it is either reducing or closing a position
+            realized_pnl_ticks += std::min(abs(position), filled_quantity) * (average_entry_price_ticks - fill_price_per_share_ticks);
+            position += filled_quantity;
+            
+            if (position > 0) {
+                average_entry_price_ticks = fill_price_per_share_ticks;
+            }
+        }
     }
-    /*
-    if increasing position
-        update position, update avg_price_per_share
-
-    if decreasing position
-        update position, update realized_pnl, handle flips
-    
-    
-    
-    */
-
-
-
-
 
     iter_order_1->second.remaining_qty -= filled_quantity;
-    if (iter_order_1->second.remaining_qty <= 0) {
+
+    if (iter_order_1->second.remaining_qty == 0) {
         order_cache.erase(order_id_1);
     }
+    else if (iter_order_1->second.remaining_qty < 0) {
+        throw std::runtime_error("There is an error in the mechanism of matching trades, quantities shouldn't go below 0");
+    }
 
-
-    
+    take_screenshot(fill_timestamp_us, false);
 }
 
-void Metrics::on_market_price_update(long long timestamp_us) {
+void Metrics::on_market_price_update(long long timestamp_us, long long best_bid, long long best_ask) {
+    current_best_bid_price_ticks = best_bid;
+    current_best_ask_price_ticks = best_ask;
 
+    take_screenshot(timestamp_us, false);
+}
+
+void Metrics::take_screenshot(long long timestamp, bool is_final) {
+    update_last_mark_price();
+
+    unrealized_pnl_ticks = position * (last_mark_price_ticks - average_entry_price_ticks);
+    total_pnl_ticks = realized_pnl_ticks + unrealized_pnl_ticks;
+
+    timestamp_series.push_back(timestamp);
+    realized_pnl_ticks_series.push_back(realized_pnl_ticks);
+    unrealized_pnl_ticks_series.push_back(unrealized_pnl_ticks);
+    total_pnl_ticks_series.push_back(total_pnl_ticks);
+    market_price_ticks_series.push_back(last_mark_price_ticks);
+
+    equity_value_peak_ticks = std::max(total_pnl_ticks, equity_value_peak_ticks);
+    max_dropdown_ticks = std::min(max_dropdown_ticks, total_pnl_ticks - equity_value_peak_ticks);
+    
+    if (is_final || timestamp - last_return_bucket_start_us >= config.return_bucket_interval_us) {
+        returns_series.push_back(total_pnl_ticks - last_marked_total_pnl_ticks);
+
+        last_return_bucket_start_us = timestamp;
+        last_marked_total_pnl_ticks = total_pnl_ticks;
+    }
 }
 
 int Metrics::get_position() {
