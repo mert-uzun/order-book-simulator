46c2add050aa8d57a2322c521dac293c1bb96ccb
Mert Uzun <mert.uzun@ug.bilkent.edu.tr>
2025-10-26 00:14:54 +0300

    implemented simulate_background_dynamics for MarketEngine

--- include/MarketEngine.h
+++ include/MarketEngine.h
@@ -3,19 +3,22 @@
 #include "OrderBook.h"
 #include "Trade.h"
 #include "Strategy.h"
+#include <sys/stat.h>
+#include <vector>
 
 class MarketEngine {
     private:
         OrderBook orderbook;
         Strategy strategy;
 
+        static const double tick_size;
+
         long long market_price_ticks;
         long long spread;
         double volatility;
         double fill_probability;
-
     public:
-        MarketEngine(long long strategy_quote_size = 1, long long strategy_tick_offset= 1, long long strategy_max_inv = 10, long long strategy_cancel_threshold = 1, long long strategy_cooldown_between_requotes = 1);
+        MarketEngine(long long strategy_quote_size = 1, long long strategy_tick_offset= 1, long long strategy_max_inv = 10, long long strategy_cancel_threshold = 1, long long strategy_cooldown_between_requotes = 1, long long starting_mid_price = 10000, long long start_spread = 2, double start_vol = 1.0, double start_fill_prob = 0.3);
         
         void update(long long);
 
--- src/MarketEngine.cpp
+++ src/MarketEngine.cpp
@@ -1,7 +1,13 @@
 #include "../include/MarketEngine.h"
+#include <algorithm>
+#include <cmath>
+#include <random>
 
-MarketEngine::MarketEngine(long long strategy_quote_size, long long strategy_tick_offset, long long strategy_max_inv, long long strategy_cancel_threshold, long long strategy_cooldown_between_requotes) : orderbook(), strategy(orderbook, strategy_quote_size, strategy_tick_offset, strategy_max_inv, strategy_cancel_threshold, strategy_cooldown_between_requotes) {}
-        
+const double MarketEngine::tick_size = 0.001;
+
+MarketEngine::MarketEngine(long long strategy_quote_size, long long strategy_tick_offset, long long strategy_max_inv, long long strategy_cancel_threshold, long long strategy_cooldown_between_requotes, long long starting_mid_price, long long start_spread, double start_vol, double start_fill_prob) : orderbook(), 
+                            strategy(orderbook, strategy_quote_size, strategy_tick_offset, strategy_max_inv, strategy_cancel_threshold, strategy_cooldown_between_requotes), market_price_ticks(starting_mid_price), spread(start_spread), volatility(start_vol), fill_probability(start_fill_prob) {}
+
 void MarketEngine::update(long long timestamp_us) {
     simulate_background_dynamics();
 
@@ -11,7 +17,32 @@ void MarketEngine::update(long long timestamp_us) {
 }
 
 void MarketEngine::simulate_background_dynamics() {
+    std::random_device rng;
+    std::mt19937 engine(rng);
 
+    std::uniform_int_distribution<int> move(-1, 1);
+    std::uniform_real_distribution<double> jump(0.0, 1.0);
+
+    long long prev_price_ticks = market_price_ticks;
+
+    market_price_ticks += move(engine) * volatility;
+
+    // A rare jump in the market simulating news
+    if (jump(engine) < 0.001) {
+        market_price_ticks += move(engine) * (5 * volatility);
+    }
+
+    long long price_change = market_price_ticks - prev_price_ticks;
+
+    spread = std::max<long long>(1, (long long)(2 + 0.5 * volatility)); 
+
+    volatility = std::sqrt((0.1 * price_change * price_change) + 0.9 * volatility);
+
+    double base_fill = 0.002;
+    double k = 0.2;
+    double alpha = 0.1;
+
+    fill_probability = (1 - alpha) * fill_probability + alpha * (base_fill * std::exp(-k * volatility));
 }
 
 void MarketEngine::check_and_trigger_fills() {
