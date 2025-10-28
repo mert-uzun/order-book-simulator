ff291512c8f992a7d24580969440aadce1dda1ad
Mert Uzun <mert.uzun@ug.bilkent.edu.tr>
2025-10-28 18:23:33 +0300

    Implemented TEST 4: AddMultipleBuyOrders to validate the addition of multiple buy orders in the order book, ensuring correct data handling and order retrieval.

--- tests/test_orderbook.cpp
+++ tests/test_orderbook.cpp
@@ -95,6 +95,67 @@ TEST(OrderBookTest, AddSingleSellOrder) {
     EXPECT_EQ(order.tsLastUpdateUs, 1)
         << "Last update timestamp does not match. Added: " << order.tsLastUpdateUs << ", intended: 1";
 }
+
+/**
+    ============================================================
+    TEST 4: AddMultipleBuyOrders
+    ============================================================
+    PURPOSE: Adds multiple buy orders to current order book, and check if they arrive to the market with correct data
+    ============================================================
+ */
+TEST(OrderBookTest, AddMultipleBuyOrders) {
+    OrderBook orderbook;
+    long long order_id1 = orderbook.add_limit_order(true, 1000000, 10, 1);
+    long long order_id2 = orderbook.add_limit_order(true, 999999, 12, 2);
+
+    // Be sure orders are added to both data members of the orderbook
+    EXPECT_NE(order_id1, -1)
+        << "Order id is -1. Valid id is not generated, order was not added to the market.";
+    EXPECT_NE(orderbook.get_order_lookup().find(order_id1), orderbook.get_order_lookup().end())
+        << "Order id is not found in the order lookup. Order was not added to the order_lookup map.";
+    EXPECT_NE(order_id2, -1)
+        << "Order id is -1. Valid id is not generated, order was not added to the market.";
+    EXPECT_NE(orderbook.get_order_lookup().find(order_id2), orderbook.get_order_lookup().end())
+        << "Order id is not found in the order lookup. Order was not added to the order_lookup map.";
+    EXPECT_FALSE(orderbook.get_buys().empty())
+        << "Buy orders are empty. Orders were not added to the buys map.";
+
+    auto order_it1 = orderbook.get_order_lookup().find(order_id1)->second;
+    Order order1 = *std::get<1>(order_it1);
+
+    EXPECT_EQ(order1.id, order_id1)
+        << "Order ids does not match. Added: " << order1.id << ", intended: " << order_id1;
+    EXPECT_EQ(order1.isActive, true)
+        << "Order is not active. Added: " << order1.isActive << ", intended: true";
+    EXPECT_EQ(order1.isBuy, true)
+        << "Order is not a buy order. Added: " << order1.isBuy << ", intended: true";
+    EXPECT_EQ(order1.priceTick, 1000000)
+        << "Price tick does not match. Added: " << order1.priceTick << ", intended: 1000000";
+    EXPECT_EQ(order1.quantity, 10)
+        << "Quantity does not match. Added: " << order1.quantity << ", intended: 10";
+    EXPECT_EQ(order1.tsCreatedUs, 1)
+        << "Created timestamp does not match. Added: " << order1.tsCreatedUs << ", intended: 1";
+    EXPECT_EQ(order1.tsLastUpdateUs, 1)
+        << "Last update timestamp does not match. Added: " << order1.tsLastUpdateUs << ", intended: 1";
+
+    auto order_it2 = orderbook.get_order_lookup().find(order_id2)->second;
+    Order order2 = *std::get<1>(order_it2);
+
+    EXPECT_EQ(order2.id, order_id2)
+        << "Order ids does not match. Added: " << order2.id << ", intended: " << order_id2;
+    EXPECT_EQ(order2.isActive, true)
+        << "Order is not active. Added: " << order2.isActive << ", intended: true";
+    EXPECT_EQ(order2.isBuy, true)
+        << "Order is not a buy order. Added: " << order2.isBuy << ", intended: true";
+    EXPECT_EQ(order2.priceTick, 999999)
+        << "Price tick does not match. Added: " << order2.priceTick << ", intended: 999999";
+    EXPECT_EQ(order2.quantity, 12)
+        << "Quantity does not match. Added: " << order2.quantity << ", intended: 12";
+    EXPECT_EQ(order2.tsCreatedUs, 2)
+        << "Created timestamp does not match. Added: " << order2.tsCreatedUs << ", intended: 2";
+    EXPECT_EQ(order2.tsLastUpdateUs, 2)
+        << "Last update timestamp does not match. Added: " << order2.tsLastUpdateUs << ", intended: 2";
+}
 /* 
     TESTS TO ADD
     
