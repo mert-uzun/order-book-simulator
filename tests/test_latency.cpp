e37fd2d5b989559ca3c53887e9a3521a1031ca51
Mert Uzun <mert.uzun@ug.bilkent.edu.tr>
2025-10-29 16:43:18 +0300

    added a test case CustomLatencyProfileSetting for test suite LatencyQueueTest

--- tests/test_latency.cpp
+++ tests/test_latency.cpp
@@ -48,15 +48,50 @@ TEST(LatencyQueueTest, DefaultInitialization) {
 
 /**
     ============================================================
-    TEST 2: CustomLatencyProfile
+    TEST 2: CustomLatencyProfileSetting
     ============================================================
-    PURPOSE: Test setting custom latency boundaries works
+    PURPOSE: Test setting custom latency boundaries works with limitations
     ============================================================
 */
-TEST(LatencyQueueTest, CustomLatencyProfile) {
+TEST(LatencyQueueTest, CustomLatencyProfileSetting) {
     LatencyQueue latency_queue;
 
-    latency_queue.reset_latency_profile(100, 200, 40, 150, 50, 180, 120, 450, 60, 160);
+    // Set the custom values with invalid order_send_min and order_send_max.
+    EXPECT_NO_THROW(latency_queue.reset_latency_profile(100, 20, 40, 150, 50, 180, 120, 450, 60, 160))
+        << "No exceptions should be thrown even some of the given values are not valid.";
+
+    // Since given min and max for order send are not logical (100 !<= 20), their values should not be updated.
+    EXPECT_EQ(latency_queue.get_latency_boundaries().order_send_min, 50)
+        << "Order send min should be 50 initially. Result: " << latency_queue.get_latency_boundaries().order_send_min << std::endl;
+
+    EXPECT_EQ(latency_queue.get_latency_boundaries().order_send_max, 200)
+        << "Order send max should be 200 initially. Result: " << latency_queue.get_latency_boundaries().order_send_max << std::endl;
+
+    EXPECT_EQ(latency_queue.get_latency_boundaries().cancel_min, 40)
+        << "Cancel min should be 30 initially. Result: " << latency_queue.get_latency_boundaries().cancel_min << std::endl;
+
+    EXPECT_EQ(latency_queue.get_latency_boundaries().cancel_max, 150)
+        << "Cancel max should be 150 initially. Result: " << latency_queue.get_latency_boundaries().cancel_max << std::endl;
+
+    EXPECT_EQ(latency_queue.get_latency_boundaries().modify_min, 50)
+        << "Modify min should be 40 initially. Result: " << latency_queue.get_latency_boundaries().modify_min << std::endl;
+
+    EXPECT_EQ(latency_queue.get_latency_boundaries().modify_max, 180)
+        << "Modify max should be 180 initially. Result: " << latency_queue.get_latency_boundaries().modify_max << std::endl;
+
+    EXPECT_EQ(latency_queue.get_latency_boundaries().acknowledge_fill_min, 120)
+        << "Acknowledge fill min should be 100 initially. Result: " << latency_queue.get_latency_boundaries().acknowledge_fill_min << std::endl;
+
+    EXPECT_EQ(latency_queue.get_latency_boundaries().acknowledge_fill_max, 450)
+        << "Acknowledge fill max should be 400 initially. Result: " << latency_queue.get_latency_boundaries().acknowledge_fill_max << std::endl;
+        
+    EXPECT_EQ(latency_queue.get_latency_boundaries().market_update_min, 60)
+        << "Market update min should be 50 initially. Result: " << latency_queue.get_latency_boundaries().market_update_min << std::endl;
+
+    EXPECT_EQ(latency_queue.get_latency_boundaries().market_update_max, 160)
+        << "Market update max should be 150 initially. Result: " << latency_queue.get_latency_boundaries().market_update_max << std::endl;
+
+
     
 }
 
